98ee752d1238bd0f89d2f5896c0e4dd454feee33
Michael Salamon <msalamo000@citymail.cuny.edu>
2021-11-04 00:53:02 -0400

    Eye blink implemented.

--- eyeblink_all.py
+++ eyeblink_all.py
@@ -1,13 +1,7 @@
 import os
-import cv2
-import dlib
-import csv
 import numpy as np
-import pandas as pd
-from sklearn.metrics import accuracy_score
-import tensorflow as tf
-from tensorflow.keras import backend as K
-from tensorflow.keras.models import load_model
+import face_recognition
+from PIL import Image
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.vgg16 import preprocess_input
@@ -16,14 +10,21 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
-from scipy.spatial import distance as dist
-from imutils import face_utils
-import face_recognition
-from PIL import Image
 
 
 # Prepare images.
 def images_ready(all_imgs,folder_name):
+    '''
+    Convert the images to proper format to run predictions
+    on a model.
+    
+    Args:
+        all_imgs: names of all images in a directory.
+        folder_dir: directory of all_imgs.
+        
+    Returns:
+        List of images in array format.
+    '''
     result=[]
     for i in all_imgs:
         # load an image from file
@@ -39,23 +40,44 @@ def images_ready(all_imgs,folder_name):
     return result
 
 def more_tests(model, folder_dir):
-    # param: , name_csv
-    # extra_test = pd.read_csv(name_csv, header=0)
-    # fast_blink_names=extra_test['Name']
-    fast_blink_names = sorted([x for x in os.listdir(folder_dir)])
+    '''
+    Runs a prediction on model.
+    Prints the binary classification of the image in directory folder_dir.
+    
+    Args:
+        model: transfer learn VGG16 model.
+        folder_dir: directory of .png files.
+        
+    Returns:
+        1 if the eyes are open, 0 if the eyes are closed.
+    '''
+    fast_blink_names = ["p.png"]
     fast_blink_names=np.array(fast_blink_names)
     fast_blink_names=images_ready(fast_blink_names, folder_dir+"/")
     regular_3=np.array(fast_blink_names)
-    good_shape_3=np.squeeze(regular_3)
+    good_shape_3=np.squeeze(regular_3, axis=0)
     result_vgg16_2= model.predict(good_shape_3)
     count_1=0
     for i in range(0,len(result_vgg16_2)):
         if result_vgg16_2[i]>0.5:
-            count_1+=1
-    print("The model predicts",(count_1/150)*100,"% of the frames are eyes open.")
-    print("The model predicts",100-((count_1/150)*100),"% of the frames are eyes closed.")
+            count_1=1
+    if count_1==1:
+        print("Eyes open.")
+    else:
+        print("Eyes closed.")
+    return count_1
 
-def full_model_run():
+def get_model():
+    '''
+    Creates a model using VGG16.
+    
+    Args:
+        None
+        
+    Returns:
+        VGG16 model for binary classification. Upper layers
+        are frozen for transfer learning.
+    '''
     model = VGG16(include_top=False, input_shape=(224, 224, 3))
     # don't train upper layers
     for layer in model.layers:
@@ -68,3 +90,26 @@ def full_model_run():
     model = Model(inputs=model.inputs, outputs=output)
     model.compile(loss='binary_crossentropy',optimizer=RMSprop(lr=0.0001),metrics=['accuracy'])
     model.load_weights("data/Weights/all_weights_eyeblink.ckpt")
+    return model
+
+def save_crop(input_image, file_name, destination):
+    '''
+    Crop an image to only contain a persons face.
+    
+    Args:
+        input_image: name of the file to be cropped.
+        file_name: name of the new cropped image.
+        destination: directory in which pictures are located.
+        
+    Returns:
+        None
+    '''
+    full_1=destination+file_name
+    full_2=destination+input_image
+    input_image=face_recognition.load_image_file(full_2)
+    locations = face_recognition.face_locations(input_image)
+    for x in locations:
+        top, right, bottom, left = x
+        face_image = input_image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+        pil_image.save(full_1)
